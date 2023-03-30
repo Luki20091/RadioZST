5a189f704b931cc958940648d0fa6cd59d638235
Luki20091 <76406872+Luki20091@users.noreply.github.com>
2023-03-30 23:35:51 +0200

    Add files via upload

--- assets/scripts/loader.js
+++ assets/scripts/loader.js
@@ -1,6 +1,11 @@
 const loader = document.getElementById("loader");
 const menuBarContainer = document.getElementById("menuBarContainer");
+const songFormContainer = document.getElementById("songFormContainer");
+const orderFormContainer = document.getElementById("orderFormContainer");
+
 window.addEventListener("load", function () {
     loader.classList.add("loader-hidden");
     menuBarContainer.classList.add("menuBarContainer-visible");
+    songFormContainer.classList.add("songFormContainer-visible");
+    orderFormContainer.classList.add("orderFormContainer-visible");
 });
