e5aa2725ec1b5c2d7f665193d5f32b4b7e9e5431
Luki20091 <76406872+Luki20091@users.noreply.github.com>
2023-03-30 18:32:30 +0200

    Update locations.js

--- assets/scripts/locations.js
+++ assets/scripts/locations.js
@@ -4,12 +4,12 @@ locations.forEach(function (loc, index) {
 	loc.addEventListener('click', function (e) {
 		if (e.target.id == "") {
 			if (e.target.parentElement.id == "index") {
-				location.replace('https://luki20091.github.io/RadioZST');
+				location.replace('https://luki20091.github.io/RadioZST/');
 			} else {
 				location.replace('https://luki20091.github.io/RadioZST/' + e.target.parentElement.id);
 			};
 		} else if (e.target.id == "index") {
-			location.replace('https://luki20091.github.io/RadioZST');
+			location.replace('https://luki20091.github.io/RadioZST/');
 		} else {
 			location.replace('https://luki20091.github.io/RadioZST/' + e.target.id);
 		};
