5f43504fd74d215ebc49b7affba741f1bfe7718d
Luki20091 <76406872+Luki20091@users.noreply.github.com>
2023-03-30 19:51:42 +0200

    Update themes.js

--- assets/scripts/themes.js
+++ assets/scripts/themes.js
@@ -2,15 +2,7 @@ document.getElementById("theme").addEventListener("click", changeTheme);
 
 function changeTheme() {
     const currentTheme = localStorage.getItem("theme");
-
     const typeTheme = currentTheme === "dark" ? "light" : "dark";
-    document.body.setAttribute('data-theme', typeTheme);
-    
-    if (typeTheme == "light") {
-        document.getElementById("theme").title = "Kliknij, aby zmienić motyw na ciemny.";
-    } else {
-        document.getElementById("theme").title = "Kliknij, aby zmienić motyw na jasny.";
-    };
 
     localStorage.setItem("theme", typeTheme);
     location.reload();
