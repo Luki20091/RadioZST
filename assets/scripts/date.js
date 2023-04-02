95058a1222470baf93835695d4e822bd43b1824d
Luki20091 <76406872+Luki20091@users.noreply.github.com>
2023-04-02 14:32:32 +0200

    Update date.js

--- assets/scripts/date.js
+++ assets/scripts/date.js
@@ -1,12 +1,39 @@
+var xmlHttp;
+function srvTime() {
+	try {
+		xmlHttp = new XMLHttpRequest();
+	}
+	catch (err1) {
+		try {
+			xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+		}
+		catch (err2) {
+			try {
+				xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+			}
+			catch (eerr3) {
+				console.log("AJAX not supported");
+			}
+		}
+	}
+	xmlHttp.open('HEAD', window.location.href.toString(), false);
+	xmlHttp.setRequestHeader("Content-Type", "text/html");
+	xmlHttp.send('');
+	return xmlHttp.getResponseHeader("Date");
+}
+
+
 function getDateString() {
-	const date = new Date();
+	var st = srvTime();
+	const date = new Date(st);
 	const year = date.getFullYear();
 	const month = `${date.getMonth() + 1}`.padStart(2, '0');
 	const day = `${date.getDate()}`.padStart(2, '0');
 	return `${day}.${month}.${year}`
 };
 function getDateString2() {
-	const date = new Date();
+	var st = srvTime();
+	const date = new Date(st);
 	const month = `${date.getMonth() + 1}`.padStart(2, '0');
 	const day = `${date.getDate()}`.padStart(2, '0');
 	const hours = `${date.getHours()}`.padStart(2, '0');
@@ -14,7 +41,8 @@ function getDateString2() {
 	return `${hours}:${minutes} - ${day}.${month}`
 };
 function getDateString3() {
-	const date = new Date();
+	var st = srvTime();
+	const date = new Date(st);
 	const year = date.getFullYear();
 	const month = `${date.getMonth() + 1}`.padStart(2, '0');
 	const day = `${date.getDate()}`.padStart(2, '0');
@@ -24,21 +52,24 @@ function getDateString3() {
 	return `${day}.${month}.${year} - ${hours}:${minutes}:${seconds}`
 };
 function getDateString4() {
-	const date = new Date();
+	var st = srvTime();
+	const date = new Date(st);
 	const hours = `${date.getHours()}`.padStart(2, '0');
 	const minutes = `${date.getMinutes()}`.padStart(2, '0');
 	const seconds = `${date.getSeconds()}`.padStart(2, '0');
 	return `${hours}:${minutes}:${seconds}`
 };
 function getDateString5() {
-	const date = new Date();
+	var st = srvTime();
+	const date = new Date(st);
 	const year = date.getFullYear();
 	const month = `${date.getMonth() + 1}`.padStart(2, '0');
 	const day = `${date.getDate()}`.padStart(2, '0');
 	return `${day}-${month}-${year}`
 };
 function getDateInt() {
-	const date = new Date();
+	var st = srvTime();
+	const date = new Date(st);
 	const hours = `${date.getHours()}`.padStart(2, '0');
 	const minutes = `${date.getMinutes()}`.padStart(2, '0');
 	const seconds = `${date.getSeconds()}`.padStart(2, '0');
@@ -46,7 +77,8 @@ function getDateInt() {
 };
 
 function getDateString6() {
-	const date = new Date();
+	var st = srvTime();
+	const date = new Date(st);
 	const year = date.getFullYear();
 	return `${year}-${Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24)}`;
 };
